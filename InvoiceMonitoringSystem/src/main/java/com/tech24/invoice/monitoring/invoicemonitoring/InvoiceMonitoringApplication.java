1fe0686ee1ae062b509189cd8e630b0783ecbdec
Mark Ortiz <ortizmark905@gmail.com>
2017-09-02 08:50:48 +0800

    Initial Commit Spring Boot Project

---
+++ InvoiceMonitoringSystem/src/main/java/com/tech24/invoice/monitoring/invoicemonitoring/InvoiceMonitoringApplication.java
@@ -0,0 +1,12 @@
+package com.tech24.invoice.monitoring.invoicemonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceMonitoringApplication.class, args);
+	}
+}
